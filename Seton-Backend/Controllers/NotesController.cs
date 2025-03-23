61a582cc588bbde311b6937f97d1aadb8386128e
pakidzisca <pakidzisca@DESKTOP-RUTCUVL>
2025-03-23 21:44:59 +0100

    test

---
+++ Seton-Backend/Controllers/NotesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seton_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/notes")]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Notes_cs> GetNote()
+        {
+            var note = new Notes_cs
+            {
+                test = "test",
+                first = "first"
+            };
+
+            return Ok(note); // Отправляем объект в JSON-формате
+        }
+    }
+}
---
+++ Seton-Backend/Notes.cs
@@ -0,0 +1,8 @@
+﻿namespace Seton_Backend
+{
+    public class Notes_cs
+    {
+        public string test {  get; set; }
+        public string first { get; set; }
+    }
+}
